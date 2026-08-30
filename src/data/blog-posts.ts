e9b7105ad0d1a8bda9ad70788c5a1db80aa164c0
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-30 19:42:56 +0530

    Automate blog reading time (#30)
    * feat: add shared reading time calculator
    
    * build: allow shared JavaScript utility
    
    * feat: derive blog reading time from content
    
    * build: apply derived reading time to prerendered articles
    
    * build: derive reading time after prerender

--- src/data/blog-posts.ts
+++ src/data/blog-posts.ts
@@ -1,6 +1,10 @@
 import { blogPosts as sourceBlogPosts, type BlogPost } from './blogs'
+import { calculateReadingTime } from '../lib/reading-time.js'
 
-export const blogPosts: BlogPost[] = sourceBlogPosts
+export const blogPosts: BlogPost[] = sourceBlogPosts.map((post) => ({
+  ...post,
+  readingTime: calculateReadingTime(post.content),
+}))
 
 export function getBlogPost(slug: string) {
   return blogPosts.find((post) => post.slug === slug)
